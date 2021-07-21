28bf2df5af4b897dabaf76219e692cd4db8b72d3
Maher MASTOURI <maher.mastouri@gmail.com>
2021-07-21 17:11:05 +0100

    v1.2

--- Src/stm32_lpm_if.c
+++ Src/stm32_lpm_if.c
@@ -3,18 +3,6 @@
   * @file    stm32_lpm_if.c
   * @author  MCD Application Team
   * @brief   Low layer function to enter/exit low power modes (stop, sleep)
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
-  *
-  ******************************************************************************
   */
 
 /* Includes ------------------------------------------------------------------*/
